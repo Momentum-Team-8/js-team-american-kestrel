19341e594dbe3e0f32ebc2eb084609039f7d1433
Middleway13 <middlewayonlinemedia@gmail.com>
2021-06-01 15:48:17 -0400

    two files modified

--- main.js
+++ main.js
@@ -2,8 +2,23 @@
 const url = 'http://localhost:3000/movies'
 
 // Create a function that lists the movies.
-// function movieList() {
-//     fetch(url)
+function movieList() {
+    fetch(url, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            title: movieText,
+            body: movieText,
+            create_at: moment().format() 
+        })
+    })
+    .then(response => response.json())
+    .then(data => renderMovieItem(data))
+}
+
+
+
+
 //     .then((response) => response.json())
 //     .then((data) => {
 //         for (let movie of data) {
@@ -13,9 +28,10 @@ const url = 'http://localhost:3000/movies'
 // }
 
 // Create a function that creates a movie. 
-function createMovie() {
-const  movieItem = document.createElement("li")
-movieItem.setAttribute (".id",)
-}
+// const createMovie = () 
+// {
+// const  movieItem = document.createElement("li")
+// movieItem.setAttribute("id", movies.id)
+// }
 
 // Create addEventListener to form submit button.
