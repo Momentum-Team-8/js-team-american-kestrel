21632619b107597b0a9d75aaaeb506fd345e4fd0
Greg Sronce <greg.sronce@gmail.com>
2021-06-01 14:45:35 -0400

    Added GET request to JS and created ul on html.

---
+++ main.js
@@ -0,0 +1,18 @@
+// Create variable where we assign the url to the variable.
+const url = 'http://localhost:3000/movies'
+
+// Create a function that lists the movies.
+// function movieList() {
+//     fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => {
+//         for (let movie of data) {
+//         renderMovieList(movie)
+//     }
+// })
+// }
+
+// Create a function that creates a movie. 
+
+
+// Create addEventListener to form submit button.
